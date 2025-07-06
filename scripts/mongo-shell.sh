3a57527e0c2be26e2701d2520aae6aef72280802
JustinTien <thitbbeb+git-commit@gmail.com>
2025-07-06 20:29:58 +0800

    fix: :beetle: scripts/mongo-shell.sh

--- scripts/mongo-shell.sh
+++ scripts/mongo-shell.sh
@@ -4,7 +4,7 @@
 # 這裡假設你的 docker-compose.yml 中的 MongoDB 服務名稱是 'mongodb'
 # 容器名稱通常會是 <專案名稱>-mongodb-1
 # 你可以透過 `docker ps --format "{{.Names}}"` 找到確切的名稱
-MONGO_CONTAINER_NAME=$(docker-compose ps -q mongodb)
+MONGO_CONTAINER_NAME=$(docker compose ps -q mongodb)
 
 if [ -z "$MONGO_CONTAINER_NAME" ]; then
   echo "錯誤：找不到 'mongodb' 服務的運行容器。請確認 Docker Compose 服務已啟動 (docker-compose up)。"
